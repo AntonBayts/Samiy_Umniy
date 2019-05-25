3ac65b52e886adc1db18fa53c408ceb93a29b966
ArtyomAdov <artemadov@mail.ru>
2019-05-25 18:56:13 +0700

    Fill function logic main and update game_process.h

--- src/Game_Process.c
+++ src/Game_Process.c
@@ -1,6 +1,13 @@
 #include Game_Process.h
-void LogicMain()
+void LogicMain(int quantity_players)
 {
-    AddPlayer();
+    int final_location = 25, board[25];
     FillBoard();
+    AddPlayers(quantity_players);
+    WhatFirst(player, quantity_players);
+    while (player[nickname][1] < final_loacation) //заменить никнейм на правильное поле
+    {
+        MovementPlayers(player, board, quantity_players);
+    }
+    GameOver(player);
 }
--- src/Game_Process.h
+++ src/Game_Process.h
@@ -4,9 +4,9 @@
 void LogicMain(int);
 void FillBoard();
 void PrintBoard();
-void AddPlayers(int **);
-void PrintPlayers(); //для проверки массива игроков в дальнейшем будет удалено
-void WhatFirst();    //возможно сделать инт с ретерном на итое которое номер игрока для мувемента
+void AddPlayers(int);
+void PrintPlayers(int **);   //для проверки массива игроков в дальнейшем будет удалено
+void WhatFirst(int **, int); //возможно сделать инт с ретерном на итое которое номер игрока для мувемента
 void RollDice();
-void MovementPlayers();
-void GameOver();
+void MovementPlayers(int **, int *, int);
+void GameOver(int **);
