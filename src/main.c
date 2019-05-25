1a18a8ac285d79cba8abc25220813e665bbe3c7c
AntonBayts <lokatheman@yandex.ru>
2019-05-25 18:08:08 +0700

    Add main.c

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "Game_Process.h"
+#include "menu.h"
+#include "print_board.h"
+
+int main {
+	int quantityplayers;
+	Menu(quantityplayers);
+	LogicMain(quantityplayers);
+	return 0;
+}
