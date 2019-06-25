8d765641b553fe21c217564ba5141859efd24dc4
ArtyomAdov <artemadov@mail.ru>
2019-06-25 20:59:14 +0700

    Create primitive gitignore

--- src/Game_Process.c
+++ src/Game_Process.c
@@ -1,27 +1,16 @@
-#include Game_Process.h
-void LogicMain(int quantity_players)
+#include "Game_Process.h"
+int **player;// будет удалено после добавления меню
+void FillBoard(int board[])
 {
-    int final_location = 25, board[25];
-    FillBoard();
-    AddPlayers(quantity_players);
-    WhatFirst(player, quantity_players);
-    while (player[nickname][1] < final_loacation) //Р·Р°РјРµРЅРёС‚СЊ РЅРёРєРЅРµР№Рј РЅР° РїСЂР°РІРёР»СЊРЅРѕРµ РїРѕР»Рµ
-    {
-        MovementPlayers(player, board, quantity_players);
-    }
-    GameOver(player);
-}
-void FillBoard()
-{
-    for (int i = 0, i < 25, i++)
+    for (int i = 0; i < 25; i++)
     {
         board[i] = i++;
     }
 }
-void AddPlayers()
+void AddPlayers(int quantity_players)
 {
-    player = (int **)malloc(5 * sizeof(int *));
-    for (int i = 0; i < QuantityPlayers; i++)
+    player = (int **)malloc(5 * sizeof(int *));//массив и там и там уменьшить на единицу
+    for (int i = 0; i < 4/*quantity_players*/; i++)//заменить четыре на кол-во игроков
     {
         player[i] = (int *)malloc(3 * sizeof(int));
         for (int j = 0; j < 2; j++)
@@ -30,4 +19,18 @@ void AddPlayers()
             player[i][1] = 0;
         };
     };
+}
+void LogicMain(int quantity_players)
+{
+    int final_location = 25, board[25],number_player=0;
+    FillBoard(board);
+    AddPlayers(quantity_players);
+    number_player=WhatFirst(player, quantity_players);
+    while (player[number_player][1] <= final_location) //Р·Р°РјРµРЅРёС‚СЊ РЅРёРєРЅРµР№Рј РЅР° РїСЂР°РІРёР»СЊРЅРѕРµ РїРѕР»Рµ
+    {
+        MovementPlayers(player, board, quantity_players,number_player);
+        number_player++;
+    }
+    number_player--;
+    GameOver(number_player);
 }
--- src/Game_Process.h
+++ src/Game_Process.h
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 void LogicMain(int);
-void FillBoard();
+void FillBoard(int*);
 void PrintBoard();
 void AddPlayers(int);
 void PrintPlayers(int **);   //для проверки массива игроков в дальнейшем будет удалено
-void WhatFirst(int **, int); //возможно сделать инт с ретерном на итое которое номер игрока для мувемента
+int WhatFirst(int **, int); //возможно сделать инт с ретерном на итое которое номер игрока для мувемента
 void RollDice();
-void MovementPlayers(int **, int *, int);
-void GameOver(int **);
+void MovementPlayers(int **, int *, int, int);
+void GameOver(int);
