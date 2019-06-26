e1ee79aca20bbab765b6370ee866afdf4316effc
ArtyomAdov <artemadov@mail.ru>
2019-06-26 16:30:05 +0700

    Add ignore for swp file and transfer gitignore

--- src/Game_Process.c
+++ src/Game_Process.c
@@ -1,5 +1,5 @@
 #include "Game_Process.h"
-int **player;// будет удалено после добавления меню
+int **player; // пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ
 void FillBoard(int board[])
 {
     for (int i = 0; i < 25; i++)
@@ -9,8 +9,8 @@ void FillBoard(int board[])
 }
 void AddPlayers(int quantity_players)
 {
-    player = (int **)malloc(5 * sizeof(int *));//массив и там и там уменьшить на единицу
-    for (int i = 0; i < 4/*quantity_players*/; i++)//заменить четыре на кол-во игроков
+    player = (int **)malloc(5 * sizeof(int *));      //пїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅ пїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+    for (int i = 0; i < 4 /*quantity_players*/; i++) //пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅ-пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
     {
         player[i] = (int *)malloc(3 * sizeof(int));
         for (int j = 0; j < 2; j++)
@@ -22,13 +22,13 @@ void AddPlayers(int quantity_players)
 }
 void LogicMain(int quantity_players)
 {
-    int final_location = 25, board[25],number_player=0;
+    int final_location = 25, board[25], number_player = 0;
     FillBoard(board);
     AddPlayers(quantity_players);
-    number_player=WhatFirst(player, quantity_players);
+    number_player = WhatFirst(player, quantity_players);
     while (player[number_player][1] <= final_location) //Р·Р°РјРµРЅРёС‚СЊ РЅРёРєРЅРµР№Рј РЅР° РїСЂР°РІРёР»СЊРЅРѕРµ РїРѕР»Рµ
     {
-        MovementPlayers(player, board, quantity_players,number_player);
+        MovementPlayers(player, board, quantity_players, number_player);
         number_player++;
     }
     number_player--;
