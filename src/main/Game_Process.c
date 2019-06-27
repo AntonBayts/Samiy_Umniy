4b1c30721598d919148fd24df0e4ec406e0df6aa
AntonBayts <lokatheman@yandex.ru>
2019-06-28 05:04:23 +0700

    Update code for Travis CI

--- src/main/Game_Process.c
+++ src/main/Game_Process.c
@@ -17,66 +17,55 @@ int Trow_roll()
     k = rand() % (6 - 1 + 1) + 1;
     return k;
 }
-int MovementPlayers(int **player, int quantity_players, int number_player)
+int MovementPlayers(int** player, int quantity_players, int number_player)
 {
     int k, i, m;
-    while (1)
-    {
+    while (1) {
         printf("PLAYER'S MOVE: %d", player[number_player][0]);
         printf("                         Input 1)THROW or 2)EXIT: ");
         scanf("%d", &i);
-        if (i == 1)
-        {
+        if (i == 1) {
             m = Trow_roll();
-            while (m > 0)
-            {
-                if (player[number_player][1] < 25)
-                {
+            while (m > 0) {
+                if (player[number_player][1] < 25) {
                     player[number_player][1]++;
                     m--;
-                }
-                else
+                } else
                     break;
                 otris(player, quantity_players);
                 sleep(1);
             }
             k = 1;
             return k;
-        }
-        else
-        {
-            if (i == 2)
-            {
+        } else {
+            if (i == 2) {
                 k = 0;
                 return k;
-            }
-            else
-            {
+            } else {
                 printf("       Error!\n");
             }
         }
     }
 }
 void check(
-    int **player,
-    int number_player,
-    int *Skip_stroke,
-    char *questions[10],
-    char *answers[10],
-    int quantity_players)
+        int** player,
+        int number_player,
+        int* Skip_stroke,
+        char* questions[10],
+        char* answers[10],
+        int quantity_players)
 {
     int i, j, k, p, a, f;
     char inp[80];
-    if (player[number_player][1] == 4 || player[number_player][1] == 14 || player[number_player][1] == 21)
-    {
+    if (player[number_player][1] == 4 || player[number_player][1] == 14
+        || player[number_player][1] == 21) {
         Skip_stroke[number_player]++;
         printf("                          Skip Stroke!\n");
         sleep(1.5);
         system("clear");
     }
     k = (player[number_player][1] + 1) % 3;
-    if (k == 0 && player[number_player][1] != 14)
-    {
+    if (k == 0 && player[number_player][1] != 14) {
         system("clear");
         p = player[number_player][1] / 3;
         printf("%s\n", questions[p]);
@@ -87,80 +76,64 @@ void check(
         f = strlen(answers[p]);
         f = f + 1;
         j = 1;
-        if (a == f)
-        {
+        if (a == f) {
             j = 0;
-            for (i = 0; answers[p][i] != '\0'; i++)
-            {
+            for (i = 0; answers[p][i] != '\0'; i++) {
                 if (inp[i] != answers[p][i])
                     j++;
             }
         }
-        if (j == 0)
-        {
+        if (j == 0) {
             printf("Good job!\n");
             sleep(1);
-            for (i = 0; i < rand() % (3 - 1 + 1) + 1; i++)
-            {
-                if (player[number_player][1] < 25)
-                {
+            for (i = 0; i < rand() % (3 - 1 + 1) + 1; i++) {
+                if (player[number_player][1] < 25) {
                     player[number_player][1]++;
                     // printf("Good job!\n");
                     otris(player, quantity_players);
                     sleep(1);
-                }
-                else
+                } else
                     break;
             }
         }
-        if (j != 0 || a != f)
-        {
+        if (j != 0 || a != f) {
             printf("It's so bad(\n");
             sleep(1);
-            for (i = 0; i < rand() % (3 - 1 + 1) + 1; i++)
-            {
-                if (player[number_player][1] > 0)
-                {
+            for (i = 0; i < rand() % (3 - 1 + 1) + 1; i++) {
+                if (player[number_player][1] > 0) {
                     player[number_player][1]--;
                     // printf("Good job!\n");
                     otris(player, quantity_players);
                     sleep(1);
-                }
-                else
+                } else
                     break;
             }
         }
     }
 }
-void LogicMain(int quantity_players, char *questions[10], char *answers[10])
+void LogicMain(int quantity_players, char* questions[10], char* answers[10])
 {
     int number_player = 1, **player, z, i, l, *Skip_stroke;
     number_player = WhatFirst(quantity_players);
-    player = (int **)malloc(4 * sizeof(int *));
-    for (int i = 0; i < quantity_players; i++)
-    {
-        player[i] = (int *)malloc(2 * sizeof(int));
+    player = (int**)malloc(4 * sizeof(int*));
+    for (int i = 0; i < quantity_players; i++) {
+        player[i] = (int*)malloc(2 * sizeof(int));
     }
-    for (i = 0; i < quantity_players; i++)
-    {
+    for (i = 0; i < quantity_players; i++) {
         player[i][0] = i + 1;
         player[i][1] = 0;
     }
-    Skip_stroke = (int *)malloc(quantity_players * sizeof(int));
-    for (i = 0; i < quantity_players; i++)
-    {
+    Skip_stroke = (int*)malloc(quantity_players * sizeof(int));
+    for (i = 0; i < quantity_players; i++) {
         Skip_stroke[i] = 0;
     }
     number_player = WhatFirst(quantity_players);
-    while (1)
-    {
+    while (1) {
         otris(player, quantity_players);
-        if (Skip_stroke[number_player] == 0)
-        {
+        if (Skip_stroke[number_player] == 0) {
             z = MovementPlayers(player, quantity_players, number_player);
         }
-        if (Skip_stroke[number_player] > 0)
-        {
+        if (Skip_stroke[number_player] > 0) {
             system("clear");
             Skip_stroke[number_player]--;
             number_player++;
@@ -171,10 +144,8 @@ void LogicMain(int quantity_players, char *questions[10], char *answers[10])
         }
         if (z == 0)
             break;
-        if (z == 1)
-        {
-            if (player[number_player][1] < 25)
-            {
+        if (z == 1) {
+            if (player[number_player][1] < 25) {
                 check(player,
                       number_player,
                       Skip_stroke,
@@ -184,12 +155,9 @@ void LogicMain(int quantity_players, char *questions[10], char *answers[10])
                 number_player++;
                 if (number_player >= quantity_players)
                     number_player = 0;
-            }
-            else
-            {
+            } else {
                 otriswin(player, quantity_players);
-                while (1)
-                {
+                while (1) {
                     printf("                 Enter 1 to exit "
                            "the menu: ");
                     scanf("%d", &l);
--- src/test/test.c
+++ src/test/test.c
@@ -5,10 +5,22 @@
 #include <menu.h>
 #include <otris.h>
 
-CTEST(GAME_PROCESS, TROW_ROLL) { ASSERT_INTERVAL(1, 6, Trow_roll()); }
+CTEST(GAME_PROCESS, TROW_ROLL)
+{
+    ASSERT_INTERVAL(1, 6, Trow_roll());
+}
 
-CTEST(GAME_PROCESS, WHATFIRST_1) { ASSERT_EQUAL(0, WhatFirst(1)); }
+CTEST(GAME_PROCESS, WHATFIRST_1)
+{
+    ASSERT_EQUAL(0, WhatFirst(1));
+}
 
-CTEST(GAME_PROCESS, WHATFIRST_RAND) { ASSERT_INTERVAL(0, 3, WhatFirst(4)); }
+CTEST(GAME_PROCESS, WHATFIRST_RAND)
+{
+    ASSERT_INTERVAL(0, 3, WhatFirst(4));
+}
 
-int main(int argc, const char **argv) { return ctest_main(argc, argv); }
+int main(int argc, const char** argv)
+{
+    return ctest_main(argc, argv);
+}
